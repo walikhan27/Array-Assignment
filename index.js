44317341f451bb816026b308e2982bc7f104e318
WALI KHAN <wk3075507@gmail.com>
2025-01-16 13:44:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const cities = [
+    "Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan","Hyderabad", "Gujranwala", 
+    "Peshawar", "Quetta","Islamabad", "Sialkot", "Bahawalpur", "Sukkur","Larkana", 
+    "Mingora", "Sahiwal", "Dera Ghazi Khan","Mirpur Khas", "Chiniot", "Okara"];
+  
+  if (cities.includes("Islamabad")) {
+    alert("Islamabad is the cleanest city of Pakistan!");
+  }
+  
